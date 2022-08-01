001ab80d892a0dc953a1272735205bb2041a3321
MaxMoir <kooksaga@gmail.com>
2022-07-31 17:40:02 -0700

    test

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+ 
+ function Loading(){
+ return (<div id="loading">
+    <h2 className="message">Searching...</h2>
+</div>
+);
+}
+
+export default Loading
---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+/**
+ * The index.js file is used to re-export from our separate files, that way rather than write:
+ * 
+ * import Feature from './components/Feature';
+ * 
+ * we can write:
+ * 
+ * import { Feature } from './components';
+ * 
+ * since index.js is assumed as part of the import when you specify a folder
+ * 
+ * Re-export Feature, Loading, Preview, Search, and Title from their respective files
+ */
+
+
+//import { Feature} from './Feature.js'
+import { Loading} from './Loading.js'
+//import { Preview} from './Preview.js'
+//import { Search} from './Search.js'
+import { Title} from './Title.js'
+
+//export { default as Feature } from './Feature'
+export { default as Loading} from './Loading'
+//export { default as Preview} from '/Preview'
+//export { default as Search} from './Search'
+export { default as Title} from './Title'
+
